8dc367d7d2f6e7af2bcc2b5782f704c2f4f18565
HHLhhx <923511708@qq.com>
2025-12-06 11:16:19 +0800

    feat: init

---
+++ src/main/java/com/nju/comment/backend/CommentConsistencyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nju.comment.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentConsistencyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommentConsistencyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nju/comment/backend/CommentConsistencyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nju.comment.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentConsistencyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
